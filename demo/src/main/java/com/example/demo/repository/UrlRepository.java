b0666f2f0007eb0320ac9effa67e887e299bc026
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-04-25 20:09:18 +0530

    Repository interface created
    implementing CRUD operations for generating a query and checking if a record exists (true/false)

--- demo/src/main/java/com/example/demo/repository/UrlRepository.java
+++ demo/src/main/java/com/example/demo/repository/UrlRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.repository;
 import com.example.demo.model.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UrlRepository extends JpaRepository<UrlMapping, Long> {
-    UrlMapping findByShortUrl(String shortUrl);
-    UrlMapping findByLongUrl(String longUrl);
+public interface UrlRepository extends MongoRepository<Url,String> {
+    Url findByShortUrl(String shortUrl);
+    boolean existsByShortUrl(String shortUrl);
 }
