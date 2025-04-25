2c1da539419f0ecc9d5ba965f04fe7bb143ab3ca
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-04-25 20:03:30 +0530

    Implemented DTO class
    for security, flexibility in API design

--- demo/src/main/java/com/example/demo/dto/UrlDto.java
+++ demo/src/main/java/com/example/demo/dto/UrlDto.java
@@ -1,4 +1,33 @@
 package com.example.demo.dto;
 
+import java.time.LocalDateTime;
+
 public class UrlDto {
+    private String url;
+    private LocalDateTime expirationDate;
+
+    private UrlDto(){
+
+    }
+
+    public UrlDto(String url, LocalDateTime expirationDate) {
+        this.url = url;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDateTime expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
