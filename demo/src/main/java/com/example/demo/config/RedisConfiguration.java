0e541166d2447af2aa2d96cd73aa0c4d60c31bc1
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-04-27 09:48:08 +0530

    Redis Configurations setup

--- demo/src/main/java/com/example/demo/config/RedisConfiguration.java
+++ demo/src/main/java/com/example/demo/config/RedisConfiguration.java
@@ -1,8 +1,20 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration
+@Configuration //defines beans
 public class RedisConfiguration {
-
+    @Bean //tells spring to manage the returned object as a bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory){ //RedisConnectionFactory - manages connections to redis
+        RedisTemplate<String,String> template = new RedisTemplate<>();//new instance
+        template.setConnectionFactory(connectionFactory);//links the template to the redis server
+        template.setKeySerializer(new StringRedisSerializer());//converts redis keys (always String) to byte arrays for storage
+        template.setValueSerializer(new GenericToStringSerializer<>(Long.class));//converts the counter value (Long) to String before storing in Redis
+        return template;
+    }
 }
--- demo/src/main/java/com/example/demo/controller/UrlController.java
+++ demo/src/main/java/com/example/demo/controller/UrlController.java
@@ -1,4 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UrlDto;
+import com.example.demo.service.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequestMapping("api/url")
 public class UrlController {
+    @Autowired
+    private UrlService urlService;
+
+    @PostMapping("/shorten")
+    public ResponseEntity<String> createShortUrl(@RequestBody UrlDto urlDto){
+        String shortUrl = urlService.createShortUrl(urlDto);
+        return ResponseEntity.ok(shortUrl);
+    }
+
+    @GetMapping("/{shortUrl}")
+    public RedirectView redirectToOriginalUrl(@PathVariable String shortUrl){
+        String originalUrl = urlService.getOriginalUrl(shortUrl);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(originalUrl);
+        return redirectView;
+    }
+
+    @GetMapping("/info/{shortUrl}")
+    public ResponseEntity<UrlDto> getUrlInfo(@PathVariable String shortUrl){
+        String originalUrl = urlService.getOriginalUrl(shortUrl);
+        UrlDto urlDto = new UrlDto(originalUrl,null);
+        return ResponseEntity.ok(urlDto);
+    }
 }
